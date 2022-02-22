9278bcac76b9c14dfb0f83b7b58ed5f39319ae7f
battlespike <95275167+battlespike@users.noreply.github.com>
2022-02-22 14:30:45 -0600

    Update getcitrixproductver.ps1

--- getcitrixproductver.ps1
+++ getcitrixproductver.ps1
@@ -1,1 +1,1 @@
-Get-WmiObject -Class Win32Reg_AddRemovePrograms -ComputerName LCAHNCRBPVS07  | where Publisher -eq "Citrix Systems, Inc." | select DisplayName, Version
+Get-WmiObject -Class Win32Reg_AddRemovePrograms -ComputerName <servername>  | where Publisher -eq "Citrix Systems, Inc." | select DisplayName, Version
